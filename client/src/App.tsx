0982a0add79f62af4b6b9c776fd9c604c75b1e50
LaxrLee <stangathagu95@gmail.com>
2023-03-29 05:35:58 +0300

    'mui_configs'

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,17 @@
-function App() {
+import { ThemeProvider } from "@emotion/react";
+import { CssBaseline } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+import { themeSettings } from "./theme";
 
-  return (
-    <div className="app">
-  
-    </div>
-  )
+function App() {
+  const theme = useMemo(() => createTheme(themeSettings), [])
+  return <div className="app">
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      hi
+    </ThemeProvider>
+  </div>;
 }
 
 export default App
