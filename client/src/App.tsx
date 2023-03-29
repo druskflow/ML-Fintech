eabe82297c32c2eef8005f43b85c51f2eba9fdef
LaxrLee <stangathagu95@gmail.com>
2023-03-29 23:10:09 +0300

    routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,26 @@
 import { ThemeProvider } from "@emotion/react";
-import { CssBaseline } from "@mui/material";
+import { Box, CssBaseline } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { useMemo } from "react";
 import { themeSettings } from "./theme";
+import Navbar from "@/scenes/navbar";
 
 function App() {
   const theme = useMemo(() => createTheme(themeSettings), [])
   return <div className="app">
+    <BrowserRouter>
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      hi
+      <Box width="100%" height="100" padding="1rem 2rem 4rem 2rem">
+        <Navbar />
+        <Routes>
+          <Route path="/" element = {<div>dashboard page</div>} />
+          <Route path="/predictions" element={<div>predictions page</div>} />
+        </Routes>
+      </Box>
     </ThemeProvider>
+    </BrowserRouter>
   </div>;
 }
 
