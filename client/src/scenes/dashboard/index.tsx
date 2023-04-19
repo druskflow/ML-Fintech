808a5060916b3e389d141dea66633993cbc7f4f1
LaxrLee <stangathagu95@gmail.com>
2023-04-19 12:05:11 +0300

    transaciton data del

--- client/src/scenes/dashboard/index.tsx
+++ client/src/scenes/dashboard/index.tsx
@@ -1,7 +1,7 @@
 import { Box, useMediaQuery } from "@mui/material";
 import Row1 from "./Row1";
 import Row2 from "./Row2";
-import Row3 from "./Row3";
+// import Row3 from "./Row3";
 
 const gridTemplateLargeScreens = `
   "a b c"
@@ -72,7 +72,7 @@ const Dashboard = () => {
     >
       <Row1 />
       <Row2 />
-      <Row3 />
+      {/* <Row3 /> */}
     </Box>
   );
 };
